24ee919794ec78774e10d111601c50afc8dbdb02
mattk5 <mattkubota5@gmail.com>
2017-05-23 18:31:26 -0400

    adjusting the header for mobile viewing

--- src/header.js
+++ src/header.js
@@ -8,14 +8,14 @@ export default class Header extends Component {
 	render() {
 		return(
 			<div className="header">
-				Matt Kubota | Web Developer 
-			<nav className="header-links">
-				<ul>
-					<li><a href="#about" className="links">About</a></li>
-					<li><a href={resume} target="_blank" className="links">Resume</a></li>
-					<li><a href="mailto:mattkubota5@gmail.com?subject=Creating%20Together!" className="links">Email</a></li>
-				</ul>
-			</nav>
+				Matt Kubota  
+				<nav className="header-links">
+					<ul>
+						<li><a href="#about" className="links">About</a></li>
+						<li><a href={resume} target="_blank" className="links">Resume</a></li>
+						<li><a href="mailto:mattkubota5@gmail.com?subject=Creating%20Together!" className="links">Email</a></li>
+					</ul>
+				</nav>
 			</div>
 			);
 	}
