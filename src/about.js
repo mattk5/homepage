e08fa898e043c256675c060add20351473a81583
mattk5 <mattkubota5@gmail.com>
2017-05-25 22:33:17 -0400

    fixed the about jump button

--- src/about.js
+++ src/about.js
@@ -7,7 +7,7 @@ export default class About extends Component {
 	render() {
 		return (
 				<div className="about-main">
-					<p className="about-title">About</p>
+					<p className="about-title"><a name="about" id="about-land">About</a></p>
 					<p className="about-body">Aloha! After discovering my passion for programming I moved to New York, leaving my life in Hawaii to pursue a career in Web Development. I completed an 18-week immersive program at Dev Bootcamp where I've learned and created programs using Ruby, Javascript & Ruby on Rails. <br/> Risking it all by leaving Retail Management in Hawaii to pursue Computer Programming in New York has been an enriching and rewarding experience. As a Web Developer, I enjoy working with the Back-End but also dabble with the Front-End as well. </p>
 				</div>
 				)
